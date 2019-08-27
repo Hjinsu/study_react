5da30a921ae01e05535e33d0daac3a8f891fb7d1
HJinsu <rlead1030@naver.com>
2019-08-28 00:24:15 +0900

    IterationComponent react

--- 5.Iteration-component_react/IterTest.js
+++ 5.Iteration-component_react/IterTest.js
@@ -10,8 +10,6 @@ class IterTest extends Component {
         this.setState({
             name : e.target.value
         });
-        console.log(this.state.name);
-        
     };
     
     //names 배열에 값을 추가하고, name 값을 초기화 
@@ -26,11 +24,34 @@ class IterTest extends Component {
         this.setState({
             names : this.state.names.concat(this.state.name),
             name : ''
-        })
-    }
+        });
+    };
+
+    handleRemove = (index) =>{
+        //편의상 name의 레퍼런스를 미리 만든다.
+        const {names} = this.state;
+         /* 
+            배열을 자르는 내장함수 slice와 전개 연산자(...)를 사용하여
+            index번째 값을 제외한 값들을 배열에 넣어준다.
+         */
+        /*
+            ES5문법 
+            this.state.names.slice(0,index).concat(this.state.names.slice(index+1,this.state.names.length))
+        */
+
+        
+        this.setState({
+            //filter로 index번째를 제외한 원소만 있는 새 배열 생성
+            /*names : names.filter((item,i) => i !== index)*/
+            names : [
+                ...names.slice(0,index),
+                ...names.slice(index + 1, names.length)
+            ]
+        });
+    };
     render() {
         const nameList = this.state.names.map(
-            (name, index) => (<li key={index}>{name}</li>)
+            (name, index) => (<li key={index} onDoubleClick={() => this.handleRemove(index)}>{name}</li>)
         )
         
         
